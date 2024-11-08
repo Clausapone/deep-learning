284609635ce89301a3e85ac0952ace367b6bae57
alematt0504 <163540211+alematt0504@users.noreply.github.com>
2024-11-08 16:13:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from torchmetrics import Accuracy, Precision, Recall, F1Score, ConfusionMatrix
 # computing and exporting the results as evaluation metrics
 def test(model, X, Y, edge_index, edge_weight, mask, criterion):
 
+    Y_test = Y[mask]
+    
     # instantiating metrics functions
     accuracy = Accuracy(task="binary")
     precision = Precision(task="binary")
@@ -18,20 +20,20 @@ def test(model, X, Y, edge_index, edge_weight, mask, criterion):
     model.eval()
     with torch.no_grad():
         preds = model.forward(X, edge_index, edge_weight)
-        loss = criterion(preds[mask], Y[mask])
-        preds = torch.round(preds)
+        loss = criterion(preds[mask], Y_test)
+        preds = torch.round(preds[mask])
 
-        accuracy(preds, Y)
-        precision(preds, Y)
-        recall(preds, Y)
-        f1_score(preds, Y)
-        conf_mat(preds, Y)
+        accuracy(preds, Y_test)
+        precision(preds, Y_test)
+        recall(preds, Y_test)
+        f1_score(preds, Y_test)
+        conf_mat(preds, Y_test)
 
         # Ottenere il valore delle metriche
-        accuracy = accuracy(preds, Y)
-        precision = precision(preds, Y)
-        recall = recall(preds, Y)
-        f1_score = f1_score(preds, Y)
-        conf_mat = conf_mat(preds, Y)
+        accuracy = accuracy(preds, Y_test)
+        precision = precision(preds, Y_test)
+        recall = recall(preds, Y_test)
+        f1_score = f1_score(preds, Y_test)
+        conf_mat = conf_mat(preds, Y_test)
 
     return loss.item(), accuracy.item(), precision.item(), recall.item(), f1_score.item(), conf_mat
